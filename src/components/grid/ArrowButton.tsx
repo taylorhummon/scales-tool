05f4e67ea1fb37dae5cf94cd019d63a63c0943ce
Taylor Hummon <other@taylorhummon.com>
2026-03-22 12:55:25 -0400

    make arrow button appear consistently across browsers

--- src/components/grid/ArrowButton.tsx
+++ src/components/grid/ArrowButton.tsx
@@ -13,17 +13,48 @@ export function ArrowButton({
   isRight,
   onClick,
   dataTestid
-}: ArrowButtonProps): JSX.Element {;
-  const arrow = isRight ? "→" : "←";
+}: ArrowButtonProps): JSX.Element {
   const isDisabled = ! onClick;
+  const backgroundColor = isDisabled ? "rgb(191, 169, 249)" : "rgb(128, 85, 245)";
+  const foregroundColor = isDisabled ? "rgb(230, 210, 255)" : "rgb(255, 255, 255)";
   return (
-    <div
+    <svg
       className={className(isDisabled)}
       onClick={onClick}
       data-testid={dataTestid}
+      viewBox="-25 -20 50 40"
+      xmlns="http://www.w3.org/2000/svg"
+      width="50px"
+      height="40px"
     >
-      {arrow}
-    </div>
+      <rect
+        x="-25"
+        y="-20"
+        width="50"
+        height="40"
+        fill={backgroundColor}
+        rx="8"
+        ry="8"
+      />
+      <g
+        transform={isRight ? "none" : "scale(-1, 1)"}
+      >
+        <line
+          x1="-8"
+          y1="0"
+          x2="7"
+          y2="0"
+          strokeWidth="2"
+          stroke={foregroundColor}
+        />
+        <polygon
+          points="8,0 5,3 5,-3"
+          strokeWidth="2"
+          stroke={foregroundColor}
+          fill={foregroundColor}
+        />
+      </g>
+    </svg>
   );
 }
 
