c5f9ab312628bbbbe5ad272b80069ce41e8427ed
Taylor Hummon <other@taylorhummon.com>
2026-03-26 11:23:02 -0400

    remove unnecessary Buttons component

--- src/components/sliders/Sliders.tsx
+++ src/components/sliders/Sliders.tsx
@@ -1,7 +1,8 @@
 import type { Dispatch, SetStateAction } from "react";
 
+import { Motion } from "src/enumerations";
 import type { State, Derived } from "src/types";
-import { Buttons } from "src/components/sliders/Buttons";
+import { Button } from "src/components/sliders/Button";
 import { ArrivingNote } from "src/components/sliders/ArrivingNote";
 import { ArrivingSolfege } from "src/components/sliders/ArrivingSolfege";
 import { NoteOnSlider } from "src/components/sliders/NoteOnSlider";
@@ -58,9 +59,41 @@ export function Sliders({
           derived={derived}
         />
       </g>
-      <Buttons
+      <Button
         derived={derived}
+        motion={Motion.DecrementDoPosition}
         setState={setState}
+        dataTestid="decrement-do-position"
+      />
+      <Button
+        derived={derived}
+        motion={Motion.IncrementDoPosition}
+        setState={setState}
+        dataTestid="increment-do-position"
+      />
+      <Button
+        derived={derived}
+        motion={Motion.DecrementKeyDegree}
+        setState={setState}
+        dataTestid="decrement-key-degree"
+      />
+      <Button
+        derived={derived}
+        motion={Motion.IncrementKeyDegree}
+        setState={setState}
+        dataTestid="increment-key-degree"
+      />
+      <Button
+        derived={derived}
+        motion={Motion.DecrementBoth}
+        setState={setState}
+        dataTestid="decrement-both"
+      />
+      <Button
+        derived={derived}
+        motion={Motion.IncrementBoth}
+        setState={setState}
+        dataTestid="increment-both"
       />
     </g>
   );
