029b47a400649d7684f73f9b6117f3a9bf02f9fd
Taylor Hummon <other@taylorhummon.com>
2026-03-26 03:10:18 -0400

    draw sharp and flat icons as svgs

--- src/components/sliders/Icon.tsx
+++ src/components/sliders/Icon.tsx
@@ -87,32 +87,54 @@ export function Icon({
   }
   if (motion === Motion.DecrementKeyDegree) {
     return (
-      <text
-        x="0"
-        y="0"
-        alignmentBaseline="central"
-        textAnchor="middle"
-        fill="black"
-        fontFamily="PT Mono"
-        fontSize="22px"
-      >
-        ♭
-      </text>
+      <>
+        <line
+          x1="-2.5"
+          y1="-8.5"
+          x2="-2.5"
+          y2="6.5"
+          strokeWidth={0.8}
+          stroke="black"
+        />
+        <path
+          fill="black"
+          stroke="black"
+          strokeWidth={1.5}
+          d="m -2.5,6.5 c 0,0 6.8039886,-3.502052 5.1449035,-5.77124323 -1.67644584,-2.29293637 -5.1005164,0.80130563 -5.1005164,0.80130563 0,0 3.23252528,-2.5530565 4.4272078,-0.62375756 C 3.3102876,3.0681666 -2.5,6.5 -2.5,6.5 Z"
+        />
+      </>
     );
   }
   if (motion === Motion.IncrementKeyDegree) {
     return (
-      <text
-        x="0"
-        y="0"
-        alignmentBaseline="central"
-        textAnchor="middle"
-        fill="black"
-        fontFamily="PT Mono"
-        fontSize="22px"
-      >
-        ♯
-      </text>
+      <>
+        <line
+          x1="1.5"
+          y1="-8.5"
+          x2="1.5"
+          y2="4.7"
+          strokeWidth={0.8}
+          stroke="black"
+        />
+        <line
+          x1="-2"
+          y1="-6.5"
+          x2="-2"
+          y2="6.7"
+          strokeWidth={0.8}
+          stroke="black"
+        />
+        <polygon
+          points="-3.5,3.5 3.5,0 3.5,0.3 -3.5,3.8"
+          strokeWidth={1.5}
+          stroke="black"
+        />
+        <polygon
+          points="-3.5,-1.5 3.5,-5 3.5,-4.7 -3.5,-1.2"
+          strokeWidth={1.5}
+          stroke="black"
+        />
+      </>
     );
   }
   if (motion === Motion.DecrementBoth) {
