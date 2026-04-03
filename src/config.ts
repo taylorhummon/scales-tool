aca3da9863024f33d49029745aaf36c2b00912f1
Taylor Hummon <other@taylorhummon.com>
2026-04-02 20:13:10 -0400

    reorder config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-// Allow up to two sharps or two flats on each note in the scale.
-export const MAX_DEGREE = 14;
-export const MIN_DEGREE = -14;
-
 // Default to Major with no sharps or flats, a.k.a. C-Major.
 export const DEFAULT_MODE = 2;
 export const DEFAULT_DEGREE = 0;
+
+// Allow up to two sharps or two flats on each note in the scale.
+export const MAX_DEGREE = 14;
+export const MIN_DEGREE = -14;
