04cdb91d26dba9094035ac200cf7b5f075b33b3e
Taylor Hummon <other@taylorhummon.com>
2026-04-03 15:10:14 -0400

    stop showing solfege on slider

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const MIN_DEGREE: number = -14;
 
 export const ALLOW_LYDIAN_LOCRIAN_LOOP: boolean = true;
 
-export const SHOW_SOLFEGE_ON_SLIDER: boolean = true;
+export const SHOW_SOLFEGE_ON_SLIDER: boolean = false;
 
 export const ROOT_DOT_STROKE_COLOR: string = "rgb(4, 51, 255)";
 export const ROOT_DOT_FILL_COLOR: string = "rgb(208, 214, 253)";
