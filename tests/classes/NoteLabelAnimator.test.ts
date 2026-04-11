7e239a6368db2278840fd9c1bafd6552e2c9ddbb
Taylor Hummon <other@taylorhummon.com>
2026-04-11 02:33:33 -0400

    update tests

---
+++ tests/classes/NoteLabelAnimator.test.ts
@@ -0,0 +1,174 @@
+import { test, expect } from "vitest";
+
+import { Motion } from "@/enumerations"
+import { NoteLabelAnimator } from "@/classes/NoteLabelAnimator";
+import { MusicalKey } from "@/classes/MusicalKey";
+
+
+test("LabelAnimation works for key degree = 0", () => {
+  const musicalKey = new MusicalKey(0, 1);
+
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).startNote.name
+  ).toBe(
+    "B"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).finishNote.name
+  ).toBe(
+    "B♭"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).isIncrement
+  ).toBe(
+    false
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).isAddingCharacter
+  ).toBe(
+    true
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).noteWithLongerName.name
+  ).toBe(
+    "B♭"
+  );
+
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).startNote.name
+  ).toBe(
+    "F"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).finishNote.name
+  ).toBe(
+    "F♯"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).isIncrement
+  ).toBe(
+    true
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).isAddingCharacter
+  ).toBe(
+    true
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).noteWithLongerName.name
+  ).toBe(
+    "F♯"
+  );
+});
+
+test("LabelAnimation works for key degree = 1", () => {
+  const musicalKey = new MusicalKey(1, 1);
+
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).startNote.name
+  ).toBe(
+    "F♯"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).finishNote.name
+  ).toBe(
+    "F"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).isIncrement
+  ).toBe(
+    false
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).isAddingCharacter
+  ).toBe(
+    false
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).noteWithLongerName.name
+  ).toBe(
+    "F♯"
+  );
+
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).startNote.name
+  ).toBe(
+    "C"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).finishNote.name
+  ).toBe(
+    "C♯"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).isIncrement
+  ).toBe(
+    true
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).isAddingCharacter
+  ).toBe(
+    true
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).noteWithLongerName.name
+  ).toBe(
+    "C♯"
+  );
+});
+
+test("LabelAnimation works for key degree = -3", () => {
+  const musicalKey = new MusicalKey(-3, 0);
+
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).startNote.name
+  ).toBe(
+    "D"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).finishNote.name
+  ).toBe(
+    "D♭"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).isIncrement
+  ).toBe(
+    false
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).isAddingCharacter
+  ).toBe(
+    true
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.DecrementDegree)).noteWithLongerName.name
+  ).toBe(
+    "D♭"
+  );
+
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).startNote.name
+  ).toBe(
+    "A♭"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).finishNote.name
+  ).toBe(
+    "A"
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).isIncrement
+  ).toBe(
+    true
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).isAddingCharacter
+  ).toBe(
+    false
+  );
+  expect(
+    (new NoteLabelAnimator(musicalKey, Motion.IncrementDegree)).noteWithLongerName.name
+  ).toBe(
+    "A♭"
+  );
+});
--- tests/utilities/state.test.ts
+++ tests/utilities/state.test.ts
@@ -18,48 +18,73 @@ test("getInitialState() works", () => {
   expect(
     getInitialState()
   ).toStrictEqual(
-    { degree: 0, root: -2, animationType: AnimationType.Simple, motion: Motion.Still }
+    {
+      degree: 0,
+      root: -2,
+      motion: Motion.Still,
+      animationType: AnimationType.Simple,
+      isUsingSolfege: false
+    }
   );
   addToBrowserHistory(new MusicalKey(0, 0));
   expect(
     getInitialState()
   ).toStrictEqual(
-    { degree: 0, root: 0, animationType: AnimationType.Simple, motion: Motion.Still }
+    {
+      degree: 0,
+      root: 0,
+      animationType: AnimationType.Simple,
+      motion: Motion.Still,
+      isUsingSolfege: false
+    }
   );
   addToBrowserHistory(new MusicalKey(2, 3));
   expect(
     getInitialState()
   ).toStrictEqual(
-    { degree: 2, root: 3, animationType: AnimationType.Simple, motion: Motion.Still }
+    {
+      degree: 2,
+      root: 3,
+      animationType: AnimationType.Simple,
+      motion: Motion.Still,
+      isUsingSolfege: false
+    }
   );
 });
 
 
 test("handleBrowserHistoryPop() works", () => {
   const state = {
-    root: 0,
     degree: 0,
-    animationType: AnimationType.Ballet,
+    root: 0,
     motion: Motion.IncrementRoot,
+    animationType: AnimationType.Ballet,
+    isUsingSolfege: false
   }
   expect(
-    handleBrowserHistoryPop(state, { root: 2, degree: 1 })
+    handleBrowserHistoryPop(state, { degree: 1, root: 2 })
   ).toStrictEqual(
-    { root: 2, degree: 1, animationType: AnimationType.Ballet, motion: Motion.Still }
+    {
+      degree: 1,
+      root: 2,
+      motion: Motion.Still,
+      animationType: AnimationType.Ballet,
+      isUsingSolfege: false
+    }
   );
 });
 
 test("musicalKeyFromHistoricalState() works", () => {
-  const musicalKey = musicalKeyFromHistoricalState({ root: 3, degree: 2 });
-  expect(
-    musicalKey.root
-  ).toBe(
-    3
-  );
+  const musicalKey = musicalKeyFromHistoricalState({ degree: 2, root: 3 });
   expect(
     musicalKey.degree
   ).toBe(
     2
+  );
+  expect(
+    musicalKey.root
+  ).toBe(
+    3
   );
 });
 
@@ -67,6 +92,6 @@ test("historicalStateFromMusicalKey() works", () => {
   expect(
     historicalStateFromMusicalKey(new MusicalKey(1, -2))
   ).toStrictEqual(
-    { root: -2, degree: 1 }
+    { degree: 1, root: -2 }
   );
 });
