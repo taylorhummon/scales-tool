e64f1731e2a4d09e443d5b5b5391d72e7b87acb3
Taylor Hummon <other@taylorhummon.com>
2026-04-13 10:54:00 -0400

    minor cleanup in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-// Default to a root note of C with no sharps or flats, a.k.a. C-Major.
-export const DEFAULT_ROOT: number = -2;
+// Default to no sharps or flats and a root note of C, a.k.a. C-Major.
 export const DEFAULT_DEGREE: number = 0;
+export const DEFAULT_ROOT: number = -2;
 
 // Allow up to two sharps or two flats on each note in the scale.
 export const MAX_DEGREE: number = 14;
